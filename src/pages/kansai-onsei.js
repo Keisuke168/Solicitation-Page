07f64a6c04136c660603ff5370fe1234ab147373
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-06-03 18:47:20 +0900

    implement table

--- src/pages/kansai-onsei.js
+++ src/pages/kansai-onsei.js
@@ -3,7 +3,7 @@ import SEO from "../components/seo"
 import "../components/layout.css"
 import Movie from "../images/kobe.mp4"
 import Layout from "../components/layout"
-
+import styled from '@emotion/styled'
 
 // styles
 const pageStyles = {
@@ -12,6 +12,21 @@ const pageStyles = {
   margin: 0,
   padding: 0,
 }
+
+
+const Title = styled.p`
+    color: rgb(102, 102, 102);
+    width: 80%;
+    margin: 5% auto;
+    text-align: center;
+    font-size: 38px;
+    border-bottom: 4px solid #66a5b3;
+    padding-bottom: 1rem;
+    @media (max-width: 768px) {
+        font-size: 18px;
+    }
+
+`
 
 // markup
 const KansaiOnsei = () => {
@@ -33,6 +48,44 @@ const KansaiOnsei = () => {
                 </div>
             
             </div>
+
+            <Title>
+                ▼全体プログラム
+            </Title>
+
+            <table border='1'>
+                <tr bgcolor='gray'>
+                    <th>開始時刻</th>
+                    <th>終了時刻</th>
+                    <th>内容</th>
+                </tr>
+                <tr>
+                    <th>12:30</th>
+                    <th>---</th>
+                    <th>受付開始</th>
+                </tr>
+                <tr>
+                    <th>13:00</th>
+                    <th>13:50</th>
+                    <th>ポスターセッション（第１セッション）</th>
+                </tr>
+                <tr>
+                    <th>14:00</th>
+                    <th>14:50</th>
+                    <th>ポスターセッション（第２セッション）</th>
+                </tr>
+                <tr>
+                    <th>15:00</th>
+                    <th>15:50</th>
+                    <th>ポスターセッション（第３セッション）</th>
+                </tr>
+                <tr>
+                    <th>13:00</th>
+                    <th>13:50</th>
+                    <th>ポスターセッション（第４セッション）</th>
+                </tr>
+                
+            </table>
         </Layout>
 
     </main>
